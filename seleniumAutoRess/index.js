1651d456a4bc8e3b8dbe452cc7287bde57750952
Greek94 <130473228+Greek94@users.noreply.github.com>
2023-04-15 06:43:45 +0800

    Update index.js

--- seleniumAutoRess/index.js
+++ seleniumAutoRess/index.js
@@ -39,24 +39,30 @@ async function login() {
       // Navigate to the Resource Field Page
       await driver.get(`${url}/dorf1.php?newdid=${villageId}`);
 
-      // Wait for 3 seconds
-      await driver.sleep(3000);
+      // Wait for 1 second
+      await new Promise(resolve => setTimeout(resolve, 3000));
 
-      // Find all upgradeable building slots
-      await driver.findElement(By.css('#resourceFieldContainer > div.level.colorLayer.good')).click();
+
+      // Wait for the upgradeable building slot to become visible
+      let element = await driver.wait(until.elementLocated(By.css('div.level.colorLayer.good')), 30000);
+
+      // Click on the upgradeable building slot
+      await element.click();
       // Click upgrade button for each building
       await driver.findElement(By.xpath('//div/button[@class="textButtonV1 green build"]')).click();
       await driver.sleep(1000);
       let upgradeBtn = await driver.wait(until.elementLocated(By.xpath("//div[@class='buildDuration']/span/a")));
       await driver.wait(until.elementIsEnabled(upgradeBtn));
       await upgradeBtn.click();
-      // Wait for 1 seconds
-      await driver.sleep(1000);
+      // Wait for 1 second
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
 
       while (true) {
         try {
           // Find all upgradeable building slots
-          let slots = await driver.findElements(By.css('#resourceFieldContainer > div.level.colorLayer.good'));
+          let slots = await driver.wait(until.elementsLocated(By.css('div.level.colorLayer.good')));
+
           if (slots.length === 0) {
             // No more upgradable building slots, exit the loop
             break;
@@ -64,18 +70,27 @@ async function login() {
           // Click upgrade button for each building
           for (let i = 0; i < slots.length; i++) {
             await slots[i].click();
-            await driver.findElement(By.xpath('//div/button[@class="textButtonV1 green build"]')).click();
-            await driver.sleep(1000);
+            // Wait for 1 second
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            // Wait until the element is located in the DOM
+            let element = await driver.wait(until.elementLocated(By.xpath('//div/button[@class="textButtonV1 green build"]')));
+            // Click the element
+            await element.click();
+
+            // Wait for 1 second
+            await new Promise(resolve => setTimeout(resolve, 1000));
             let upgradeBtn = await driver.wait(until.elementLocated(By.xpath("//div[@class='buildDuration']/span/a")));
             await driver.wait(until.elementIsEnabled(upgradeBtn));
             await upgradeBtn.click();
-            await driver.sleep(1000);
+            // Wait for 1 second
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
           }
         } catch (err) {
           console.error(err);
         }
       }
-      
+
 
     } catch (error) {
       console.error(`Error occurred while logging in with credentials ${JSON.stringify(credentials[i])}: ${error}`);
